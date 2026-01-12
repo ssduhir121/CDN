950b8124c85bc633a4060e8c58134097b203706b
sudhir405 <ssudhir5058@gmail.com>
2026-01-12 20:10:57 +0530

    message

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(cors({
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  allowedHeaders: ["Content-Type", "Authorization", "x-auth-token"]
 }));
 
 app.use(express.json());
