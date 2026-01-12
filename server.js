8d3e1c8d670a4eeb917a1595aa2355350acc065a
sudhir405 <ssudhir5058@gmail.com>
2026-01-12 20:03:57 +0530

    message

--- server.js
+++ server.js
@@ -31,8 +31,12 @@ const io = socketIo(server, {
 // }));
 
 app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true
+  origin: [
+    "https://cdn-frontend.vercel.app"
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.use(express.json());
